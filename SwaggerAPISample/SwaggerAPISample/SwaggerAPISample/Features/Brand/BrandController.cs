892b1ccd52cea04089575d8c3169dbfb7c068971
VNRESOURCE\van.nguyenminh <minhvan.996@outlook.com>
2020-12-30 17:42:49 +0700

    upate code

---
+++ SwaggerAPISample/SwaggerAPISample/SwaggerAPISample/Features/Brand/BrandController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using SwaggerAPISample.Infrastructure;
+
+namespace SwaggerAPISample.Features.Brand
+{
+    public class BrandController : ApiControllerBase
+    {
+        public BrandController(IMediator mediator) : base(mediator)
+        {
+        }
+    }
+}
---
+++ SwaggerAPISample/SwaggerAPISample/SwaggerAPISample/Infrastructure/ApiControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace SwaggerAPISample.Infrastructure
+{
+    [ApiController]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class ApiControllerBase : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        protected ApiControllerBase(IMediator mediator)
+            => _mediator = mediator;
+    }
+}
